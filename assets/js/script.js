aea0ccd13540c53c545acb13382c3d2f15cb1748
Abuzar Ghiffari <abuzarghiffari04@gmail.com>
2022-08-20 19:23:07 +0700

    ADD Kuliner and CHANGE Hero - Abuzar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,24 @@
 
 const text = document.getElementById('welcome-text')
-const greetings = ["Salamakki Kabattuanta", "Onomi Fakhai", "Sugeng Rawuh", "Saleum Teuka", "Tabea Waya", "Koe Pontam", "Salamaik Datang", "Kopisanangan Kinorikatan"]
+const greetings = ["Onomi Fakhai", "Sugeng Rawuh", "Saleum Teuka", "Tabea Waya", "Koe Pontam", "Salamaik Datang"]
 
 const myNav = document.getElementById('navbar')
 const myNavLink = document.querySelectorAll('.nav-link')
 
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    myNav.classList.add('bg-light', 'navbar-light')
+  myNav.classList.add('bg-light', 'navbar-light')
 } else {
-    window.onscroll = function () {
-        if (window.scrollY >= 55) {
-            myNav.classList.add("bg-light");
-            myNav.classList.remove("bg-transparent");
-        }
-        else {
-            myNav.classList.add("bg-transparent"); i
-            myNav.classList.remove("bg-light");
-
-        }
-    };
+  window.onscroll = function () {
+    if (window.scrollY >= 55) {
+      myNav.classList.add("bg-light");
+      myNav.classList.remove("bg-transparent");
+    } else {
+      myNav.classList.add("bg-transparent"); i
+      myNav.classList.remove("bg-light");
+    }
+  };
 }
 
 setInterval(() => {
-    text.innerText = greetings[Math.floor(Math.random() * greetings.length)]
+  text.innerText = greetings[Math.floor(Math.random() * greetings.length)]
 }, 4000);
