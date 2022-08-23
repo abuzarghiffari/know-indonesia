8c6fa17e98dd015f6783608f373434ec7a2dce74
Abuzar Ghiffari <abuzarghiffari04@gmail.com>
2022-08-23 19:19:11 +0700

    CHANGE Navbar - Abuzar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,20 @@
 
 const text = document.getElementById("welcome-text")
-const greetings = ["Onomi Fakhai", "Sugeng Rawuh", "Saleum Teuka", "Tabea Waya", "Koe Pontam", "Salamaik Datang"]
+const greetings = ["Selamat Datang", "Onomi Fakhai", "Sugeng Rawuh", "Saleum Teuka", "Tabea Waya", "Koe Pontam", "Salamaik Datang"]
 
 const myNav = document.getElementById("navbar")
-// const myNavLink = document.querySelectorAll('.nav-link')
 
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-  myNav.classList.remove("navbar-dark")
+  myNav.classList.remove("navbar-dark", "bg-transparent");
   myNav.classList.add("bg-light", "navbar-light")
 } else {
   window.onscroll = function () {
     if (window.scrollY >= 55) {
       myNav.classList.add("navbar-light", "bg-light")
-      myNav.classList.remove("navbar-dark")
+      myNav.classList.remove("navbar-dark", "bg-transparent")
     } else {
       myNav.classList.remove("navbar-light", "bg-light")
-      myNav.classList.add("navbar-dark")
+      myNav.classList.add("navbar-dark", "bg-transparent")
     }
   };
 }
